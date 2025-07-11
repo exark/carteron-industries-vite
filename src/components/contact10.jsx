5fc576c37c1924cf9164d0fb5dbd19df68fcb90c
exark <ali.ahmed.benhamouda@gmail.com>
2025-07-11 14:52:12 +0100

    props destructuring with default values for contact10

--- src/components/contact10.jsx
+++ src/components/contact10.jsx
@@ -1,27 +1,38 @@
 import React, { Fragment } from 'react'
-
 import PropTypes from 'prop-types'
-
 import './contact10.css'
 
-const Contact10 = (props) => {
+// ⬇️ On place les valeurs par défaut dans la destructuration des props
+const Contact10 = ({
+  content1,
+  location2Description,
+  location2ImageSrc,
+  location2,
+  location1ImageSrc,
+  location1Description,
+  contact10Id = '',
+  location1ImageAlt = 'Main Office Location',
+  location1,
+  heading1,
+  location2ImageAlt = 'Email Contact',
+}) => {
   return (
     <div
-      id={props.contact10Id}
+      id={contact10Id}
       className="contact10-container1 thq-section-padding"
     >
       <div className="contact10-max-width thq-section-max-width">
         <div className="contact10-content1 thq-flex-row">
           <div className="contact10-content2">
             <h2 className="thq-heading-2">
-              {props.heading1 ?? (
+              {heading1 ?? (
                 <Fragment>
                   <span className="contact10-text21">Get in Touch</span>
                 </Fragment>
               )}
             </h2>
             <p className="thq-body-large">
-              {props.content1 ?? (
+              {content1 ?? (
                 <Fragment>
                   <span className="contact10-text16">
                     Feel free to reach out for inquiries or collaborations.
@@ -34,12 +45,12 @@ const Contact10 = (props) => {
         <div className="contact10-content3 thq-flex-row">
           <div className="contact10-container2">
             <img
-              alt={props.location1ImageAlt}
+              alt={location1ImageAlt}
               src="/images/office.jpg"
               className="contact10-image1 thq-img-ratio-16-9"
             />
             <h3 className="contact10-text12 thq-heading-3">
-              {props.location1 ?? (
+              {location1 ?? (
                 <Fragment>
                   <span className="contact10-text20">
                     Address: 123 Smart Tech Street, City, Country
@@ -48,7 +59,7 @@ const Contact10 = (props) => {
               )}
             </h3>
             <p className="thq-body-large">
-              {props.location1Description ?? (
+              {location1Description ?? (
                 <Fragment>
                   <span className="contact10-text19">
                     Visit us at our main office location.
@@ -69,12 +80,12 @@ const Contact10 = (props) => {
           </div>
           <div className="contact10-container4">
             <img
-              alt={props.location2ImageAlt}
+              alt={location2ImageAlt}
               src="/images/contact-smartphone.jpg"
               className="contact10-image2 thq-img-ratio-16-9"
             />
             <h3 className="contact10-text14 thq-heading-3">
-              {props.location2 ?? (
+              {location2 ?? (
                 <Fragment>
                   <span className="contact10-text18">
                     Email: info@smarttechengineer.com
@@ -83,7 +94,7 @@ const Contact10 = (props) => {
               )}
             </h3>
             <p className="thq-body-large">
-              {props.location2Description ?? (
+              {location2Description ?? (
                 <Fragment>
                   <span className="contact10-text17">
                     Send us an email for any questions or proposals.
@@ -106,20 +117,6 @@ const Contact10 = (props) => {
       </div>
     </div>
   )
-}
-
-Contact10.defaultProps = {
-  content1: undefined,
-  location2Description: undefined,
-  location2ImageSrc:undefined,
-  location2: undefined,
-  location1ImageSrc:undefined,
-  location1Description: undefined,
-  contact10Id: '',
-  location1ImageAlt: 'Main Office Location',
-  location1: undefined,
-  heading1: undefined,
-  location2ImageAlt: 'Email Contact',
 }
 
 Contact10.propTypes = {
