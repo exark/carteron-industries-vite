dee25c8d42312a097ba4644dd18ca0ba24d7b37d
exark <ali.ahmed.benhamouda@gmail.com>
2025-07-31 13:10:57 +0100

    email api implemented

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -2,9 +2,9 @@
 // Remplacez ces valeurs par vos propres clés EmailJS
 
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: 'YOUR_SERVICE_ID', // Remplacez par votre Service ID
-  TEMPLATE_ID: 'YOUR_TEMPLATE_ID', // Remplacez par votre Template ID
-  PUBLIC_KEY: 'YOUR_PUBLIC_KEY', // Remplacez par votre Public Key
+  SERVICE_ID: 'service_1su0fhv', // Remplacez par votre Service ID
+  TEMPLATE_ID: 'template_9ywvme8', // Remplacez par votre Template ID
+  PUBLIC_KEY: 'fdc373TcQMt8_YYcC', // Remplacez par votre Public Key
 };
 
 // Template d'email recommandé pour EmailJS
