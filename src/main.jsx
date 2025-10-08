8ac85bc6101f8418ef8d9c4661a2aebbb9c06fc2
exark <ali.ahmed.benhamouda@gmail.com>
2025-10-08 15:55:12 +0100

    feat: integrate Vercel Speed Insights for performance monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './style.css'
 import './assets/i18n'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <>
+        <App />
+        <SpeedInsights />
+    </>
 )
